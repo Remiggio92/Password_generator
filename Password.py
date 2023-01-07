bf380ca5b6a0d07f0cf28cd196771e3b0dfb4574
Remigiusz <117860294+Remiggio92@users.noreply.github.com>
2023-01-07 19:37:10 +0100

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,4 +1,3 @@
-#Password Generator Project
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -8,9 +7,6 @@ print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 a = int(nr_letters)
 b = int(nr_symbols)
@@ -22,8 +18,5 @@ for x in password:
   print(f"Your password could be: " + ''.join(password))
   break
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-
 Strong_password = ''.join(sorted(password))
-print(Strong_password)
+print(f"Your strong password could be: " + Strong_password)
